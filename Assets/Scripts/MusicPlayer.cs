72169d4179c69ede0f7b96e0efadc003d579cb1a
Moaf2411 <72664602+Moaf2411@users.noreply.github.com>
2020-12-09 21:02:27 +0330

    Start Up The Project

---
+++ Assets/Scripts/MusicPlayer.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MusicPlayer : MonoBehaviour
+{
+    private AudioSource audio;
+    private void Awake()
+    {
+        DontDestroyOnLoad(this);
+        audio = GetComponent<AudioSource>();
+        audio.PlayOneShot(audio.clip);
+        Invoke("LoadFirstScene" , 5);
+        
+        
+    }
+
+    void LoadFirstScene()
+    {
+        SceneManager.LoadScene(1);
+    }
+}
