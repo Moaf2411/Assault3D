5b71ab6741ec16665704c33b8ced901db17d69bc
Moaf2411 <72664602+Moaf2411@users.noreply.github.com>
2020-12-10 21:33:00 +0330

    Movement and input system

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    private PlayerController playerController;
+    [SerializeField] 
+    private float xSpeed = 4f;
+
+
+    private void Awake()
+    {
+        playerController = GetComponent<PlayerController>();
+    }
+
+
+    private void Update()
+    {
+            float xOffset = xSpeed * playerController.movement.x * Time.deltaTime;
+            xOffset = Mathf.Clamp(xOffset + transform.localPosition.x, -3, 3);
+            transform.localPosition = new Vector3(xOffset , transform.localPosition.y , transform.localPosition.z);
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerController : MonoBehaviour
+{
+    public Vector2 movement;
+    
+   
+    
+        public void OnMoveInput(InputAction.CallbackContext context)
+        {
+            movement = context.ReadValue<Vector2>();
+        }
+        
+        
+        
+}
