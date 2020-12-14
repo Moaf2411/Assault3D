1e4e23f6927ddf1add4e85c6cfdffa546d1c20ac
Moaf2411 <72664602+Moaf2411@users.noreply.github.com>
2020-12-14 20:07:27 +0330

    little work done after a busy week.

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -6,7 +6,10 @@ public class Player : MonoBehaviour
 {
     private PlayerController playerController;
     [SerializeField] 
-    private float xSpeed = 4f;
+    private float xSpeed = 10f;
+
+    private float ySpeed = 8f;
+    
 
 
     private void Awake()
@@ -14,11 +17,40 @@ public class Player : MonoBehaviour
         playerController = GetComponent<PlayerController>();
     }
 
+    private float xOffset;
+    private float yOffset;
 
     private void Update()
     {
-            float xOffset = xSpeed * playerController.movement.x * Time.deltaTime;
-            xOffset = Mathf.Clamp(xOffset + transform.localPosition.x, -3, 3);
-            transform.localPosition = new Vector3(xOffset , transform.localPosition.y , transform.localPosition.z);
+        
+        Move();
+        Rotate();     
+                    
+    }
+
+    
+    
+    
+    
+    private void Move()
+    {
+        xOffset = xSpeed * playerController.movement.x * Time.deltaTime;
+        yOffset = ySpeed * playerController.movement.y * Time.deltaTime;
+        yOffset = Mathf.Clamp(yOffset + transform.localPosition.y, -2, 2);
+        xOffset = Mathf.Clamp(xOffset + transform.localPosition.x, -3, 3);
+        transform.localPosition = new Vector3(xOffset , yOffset , transform.localPosition.z);
+    }
+
+    private float yRot,yR;
+    private void Rotate()
+    {
+        yRot = playerController.movement.x * -1 ;
+        yR = Mathf.Clamp( yR + yRot + transform.localRotation.y , -15, 15);
+        
+        Quaternion y = Quaternion.Euler(transform.localPosition.y * -15 + playerController.movement.y * 7 , transform.localPosition.x * 10 , 0);
+        transform.localRotation = y;
+
+
+
     }
 }
